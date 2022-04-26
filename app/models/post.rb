50bcac260c728501c85e3addeb40aa4e66ba4db7
VuDej <drvu47@gmail.com>
2022-04-26 19:43:59 +0200

    views done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
+  has_many :comments
   has_many :likes
-end
 
-def update_post_counter
-  author.increment!(post_counter)
-end
+  def update_post_counter
+    author.increment!(:post_counter)
+  end
 
-def most_recent_comments
-  comments.limit(5).order(created_at: desc)
+  def most_recent_comments
+    comments.order(created_at: :desc).includes(:author).limit(5)
+  end
 end
