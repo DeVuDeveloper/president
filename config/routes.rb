469c61696a13bdc315411d85644b1a8e13695465
VuDej <drvu47@gmail.com>
2022-04-28 09:44:45 +0200

    half time

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root "users#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: %i[index new create show destroy]
+  end
+
+  resources :posts do
+    resources :comments, only: %i[create]
+    resources :likes, only: %i[create]
   end
 end
